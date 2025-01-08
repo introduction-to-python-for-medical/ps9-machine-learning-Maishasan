236bcceb77ed9e749e3cad58ee9392c61d5c2522
Maishasan <hsnmys97@gmail.com>
2025-01-08 22:27:49 +0200

    Update main.py

--- main.py
+++ main.py
@@ -13,10 +13,10 @@ scaler = MinMaxScaler()
 x = scaler.fit_transform(x)
 
 from sklearn.model_selection import train_test_split
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=55)
 
 from sklearn.tree import DecisionTreeClassifier
-model = DecisionTreeClassifier(max_depth=5)
+model = DecisionTreeClassifier(max_depth=9)
 model.fit(x_train, y_train)
 
 from sklearn.metrics import accuracy_score
