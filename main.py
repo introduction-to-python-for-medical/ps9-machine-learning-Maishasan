d820ef41db5d95ce4a3001aa5575551ac225583f
Maishasan <hsnmys97@gmail.com>
2025-01-08 22:26:44 +0200

    Update main.py

--- main.py
+++ main.py
@@ -15,8 +15,8 @@ x = scaler.fit_transform(x)
 from sklearn.model_selection import train_test_split
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
 
-from sklearn.neighbors import KNeighborsClassifier
-model =KNeighborsClassifier(kn_neighbors=5)
+from sklearn.tree import DecisionTreeClassifier
+model = DecisionTreeClassifier(max_depth=5)
 model.fit(x_train, y_train)
 
 from sklearn.metrics import accuracy_score
@@ -24,8 +24,7 @@ y_pred = model.predict(x_test)
 accuracy = accuracy_score(y_test, y_pred)
 print(f'Accuracy: {accuracy}')
 
-#Accuracy: 0.8125
+#Accuracy: 0.8425
 
-#7. Save and upload the model:
 import joblib
 joblib.dump(model, 'my_model.joblib')
